f5227f16c8b6c2fd7f0c8515fe31133ada1ba2ba
mikeeichler <86664474+mikeeichler@users.noreply.github.com>
2022-10-30 22:15:10 +0000

    more stuff

--- control.go
+++ control.go
@@ -3,10 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Printf("Binary: %b\\%b", 4, 5)
+		fmt.Println("Please provide a command line argument")
+		return
+	}
+	argument := os.Args[1]
+	switch argument {
+	case "0":
+		fmt.Println("Zero!")
+	case "1":
+		fmt.Println("One!")
+	case "2", "3":
+		fmt.Println("2 or 3!")
+		fallthrough
+	default:
+		fmt.Println("value: ", argument)
+
+	}
+	value, err := strconv.Atoi(argument)
+	if err != nil {
+		fmt.Println("Cannot convert to int:", argument)
+		return
+	}
+	switch {
+	case value == 0:
+		fmt.Println("Zero!")
+	case value > 0:
+		fmt.Println("Positive integer")
+	case value < 0:
+		fmt.Println("Negative integer")
+	default:
+		fmt.Println("This should not happen:", value)
 	}
 }
